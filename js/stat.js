854c682bfd6ac28196115f3c6f76e88c28784bcd
bervachenok <bervachenok@bn.ru>
2018-04-02 17:15:23 +0300

    Выполнил задание с выводом статистики

--- js/stat.js
+++ js/stat.js
@@ -2,26 +2,28 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+
 var CLOUD_WIDTH_SIDE = 360;
 var CLOUD_HEIGHT_SIDE = 200;
 
-var cloudAngleX = (CLOUD_WIDTH - CLOUD_WIDTH_SIDE) / 2;
+var cloudAngleX = CLOUD_WIDTH - CLOUD_WIDTH_SIDE;
 var cloudAngleY = (CLOUD_HEIGHT - CLOUD_HEIGHT_SIDE) / 2;
 
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 
 var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_GAP = 30;
 
 var TEXT_X = 100;
 var TEXT_Y = 40;
 
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
+
+  // Координаты построения восьмиугольника
   var x2 = x + CLOUD_WIDTH_SIDE;
   var x3 = x2 + cloudAngleX;
   var x4 = x - cloudAngleX;
@@ -62,18 +64,31 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-
-  renderText(ctx, "Ура вы победили!", TEXT_X, TEXT_Y);
-  renderText(ctx, "Список победителей:", TEXT_X, TEXT_Y * 1.5);
+  renderText(ctx, 'Ура вы победили!', TEXT_X, TEXT_Y);
+  renderText(ctx, 'Список победителей:', TEXT_X, TEXT_Y * 1.5);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i + TEXT_Y * 1.5);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i + TEXT_Y*2, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var saturation = getRandomInt(0, 100) + '%';
+      var blueFiller = 'hsl(240,' + saturation + ', 50%)';
+      ctx.fillStyle = blueFiller;
+    }
+
+    ctx.fillRect(CLOUD_X + GAP + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - cloudAngleY, -BAR_WIDTH, -((BAR_HEIGHT * times[i]) / maxTime));
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - cloudAngleY / 2);
   }
 };
